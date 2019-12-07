d52f1a7e7f27274a3adea4aa163612a00a8bc1c8
benayac <benaya.c@ugm.ac.id>
2019-12-07 18:06:42 +0700

    add something

--- bot.py
+++ bot.py
@@ -10,24 +10,37 @@ tweet_id = []
 
 def get_mention():
     for tweet in tweepy.Cursor(api.mentions_timeline).items():
-        if('besok' in tweet.text.split() and tweet.id not in tweet_id):
-            tweet_schedule('besok', tweet.user.screen_name, tweet.id)
+        try:
+            if('besok' in tweet.text.split() and tweet.id not in tweet_id):
+                tweet_schedule('besok', tweet.user.screen_name, tweet.id)
+                tweet_id.append(tweet.id)
+            elif('lusa' in tweet.text.split() and tweet.id not in tweet_id):
+                tweet_schedule('lusa', tweet.user.screen_name, tweet.id)
+                tweet_id.append(tweet.id) 
+        except Exception as identifier:
+            print(identifier)
             tweet_id.append(tweet.id)
-        elif('lusa' in tweet.text.split() and tweet.id not in tweet_id):
-            tweet_schedule('lusa', tweet.user.screen_name, tweet.id)
-            tweet_id.append(tweet.id)
-        time.sleep(5 * 60)  
+    waiting(300) 
 
 def get_tweet_keyword():
     tweets = api.search(q="teti2018")
     for tweet in tweets:
-        if('besok' in tweet.text.split() and tweet.id not in tweet_id):
-            tweet_schedule('besok', tweet.user.screen_name, tweet.id)
-            tweet_id.append(tweet.id)
-        elif('lusa' in tweet.text.split() and tweet.id not in tweet_id):
-            tweet_schedule('lusa', tweet.user.screen_name, tweet.id)
-            tweet_id.append(tweet.id)   
-        time.sleep(15)     
+        try:
+            if('besok' in tweet.text.split() and tweet.id not in tweet_id):
+                tweet_schedule('besok', tweet.user.screen_name, tweet.id)
+                tweet_id.append(tweet.id)
+            elif('lusa' in tweet.text.split() and tweet.id not in tweet_id):
+                tweet_schedule('lusa', tweet.user.screen_name, tweet.id)
+                tweet_id.append(tweet.id)
+            elif('ini' in tweet.text.split() and tweet.id not in tweet_id):
+                tweet_schedule('hari ini', tweet.user.screen_name, tweet.id)
+                tweet_id.append(tweet.id)
+            elif('lengkap' in tweet.text.split() and tweet.id not in tweet_id):
+                tweet_schedule_daily(tweet.user.screen_name, tweet.id) 
+                tweet_id.append(tweet.id)
+        except Exception as identifier:
+            print(identifier)
+            tweet_id.append(tweet.id)    
 
 def tweet_schedule(question, username, reply_id):
     if(question == 'besok'):    
@@ -51,14 +64,29 @@ def tweet_schedule(question, username, reply_id):
         except Exception as identifier:
             api.update_status('@%s Wah lusa gak ada UAS bro' % username, reply_id)
             print(identifier)
+    
+    elif(question == 'hari ini'):
+        try:
+            tomorrow = datetime.datetime.today()
+            tomorrow = str(tomorrow).split()[0]
+            tomorrow = datetime.datetime.strptime(tomorrow, '%Y-%m-%d')
+            api.update_status('@%s Hari ini uasnya %s Bro' % (username, jadwalUas[tomorrow]), reply_id)
+            print('@%s Hari ini uasnya %s Bro' % (username, jadwalUas[tomorrow]), reply_id)
+        except Exception as identifier:
+            api.update_status('@%s Wah hari ini gak ada UAS bro' % username, reply_id)
+            print(identifier)
 
-def tweet_schedule_daily():
-    print('Ingat UAS!')
-    print('Senin tgl 9 Basdat jam 1 \nRabu tgl 11 SO / OS jam 10 \nJumat tgl 13 SG jam 7.30 \nSabtu tgl 15 KWN jam 11.30\nSenin tgl 16 Jarkomdat jam 1\nSelasa tgl 17 Matdislog jam 10\nRabu tgl 18 ISIS jam 7.30\nKamis tgl 19 Sismik jam 1\nJumat tgl 20 Pemsim jam 8.30')
+def tweet_schedule_daily(username, reply_id):
+    message = 'Inget bro\nSenin tgl 9 Basdat jam 1 \nRabu tgl 11 SO / OS jam 10 \nJumat tgl 13 SG jam 7.30 \nSabtu tgl 15 KWN jam 11.30\nSenin tgl 16 Jarkomdat jam 1\nSelasa tgl 17 Matdislog jam 10\nRabu tgl 18 ISIS jam 7.30\nKamis tgl 19 Sismik jam 1\nJumat tgl 20 Pemsim jam 8.30'
+    api.update_status('@%s %s' % (username, message), reply_id)
+
+def waiting(second):
+    for i in range(second):
+        print("waiting... %i seconds left" % int(second - i))
+        time.sleep(1)
 
 while True:
-    try:
-        get_tweet_keyword()
-        #get_mention()
-    except Exception as e:
-        print(e)
+    get_tweet_keyword()
+    waiting(15)
+    print("Searching for tweet")
+    
