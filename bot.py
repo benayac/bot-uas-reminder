1893276e59584d60d2d1388fcf649fe71a527d6f
coolcool.rio75@gmail.com <benaya.c@ugm.ac.id>
2019-12-08 20:46:53 +0700

    debugging with heroku

--- bot.py
+++ bot.py
@@ -127,11 +127,13 @@ while True:
     date = datetime.datetime.strptime(date, "%Y-%m-%d")
     tomorrow = str(datetime.datetime.today() + datetime.timedelta(days=1)).split()[0]
     tomorrow = datetime.datetime.strptime(tomorrow, "%Y-%m-%d")
+    print("Now is " + str(tomorrow))
     try:
         if(date in jadwalUas.keys() and datetime.datetime.now().strftime("%H-%M") == '05-00'):
             daily_uas_reminder(date, 'today')
-        if(tomorrow in jadwalUas.keys() and datetime.datetime.now().strftime("%H-%M") == '20-30'):
+        if(tomorrow in jadwalUas.keys() and datetime.datetime.now().strftime("%H-%M") == '21-00'):
             daily_uas_reminder(tomorrow, 'tomorrow')
+            print('Go to if tomorrow')
         if(datetime.datetime.now().strftime("%H-%M") == '12-00'):
             api.update_status('Inget bro\nSenin tgl 9 Basdat jam 1 \nRabu tgl 11 SO / OS jam 10 \nJumat tgl 13 SG jam 7.30 \nSenin tgl 16 Jarkomdat jam 1\nSelasa tgl 17 Matdislog jam 10\nRabu tgl 18 ISIS jam 7.30\nKamis tgl 19 Sismik jam 1\nJumat tgl 20 Pemsim jam 8.30')    
         if(int(datetime.datetime.now().strftime("%M")) % 5 == 0): 
