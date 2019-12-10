1723a897f509cae3cc5d610faceb85ee74a01b21
coolcool.rio75@gmail.com <benaya.c@ugm.ac.id>
2019-12-10 11:11:32 +0700

    secrets

---
+++ secrets.py
@@ -0,0 +1,24 @@
+import datetime
+
+C_KEY = "FiZ0cb1WK25CjsfiEgmiwYZRe"  
+C_SECRET = "oN6Eds2pCemqK9V2O2VZvieZVznoLyafXltIsxgwJkrJMDiX1s"  
+A_TOKEN = "1202151771298119680-AQnsMnvGhZT15tWbHyQu9e41i6ElAP"  
+A_TOKEN_SECRET = "gmDcbjtO1BEz8gLACI1zWTGEMXgu7Kdtsw1I1tJ61BkR2"  
+
+database_api = "AIzaSyBeMfTj0j7K-rSnmZ-VUFBAPk6INz69tZg"
+
+jadwalUas = {
+    datetime.datetime(2019, 12, 17) : "Matematika Diskret dan Logika jam 10",
+    datetime.datetime(2019, 12, 18) : "Isyarat dan Sistem jam 7.30",
+    datetime.datetime(2019, 12, 19) : "Sistem Mikroprosesor jam 1",
+    datetime.datetime(2019, 12, 11) : "Sistem Operasi jam 10",
+    datetime.datetime(2019, 12, 20) : "Teknik Pemodelan dan Simulasi jam 7.30",
+    datetime.datetime(2019, 12, 9) : "Teknik Basis Data jam 1",
+    datetime.datetime(2019, 12, 13) : "Studium Generale jam 7.30",
+    datetime.datetime(2019, 12, 16) : "Jaringan dan Komunikasi Data jam 1",
+}
+
+howwto = 'Bot TETI18 edisi twitter lur\nCara pakenya sebutkan keyword trus tanya aja mau jadwal hari ini, besok atau jadwal lengkap\nKhusus followers:\nDaily reminder h-1 dan hari h uas lho!\nAyo yg kesepian gak ada yg ngingetin uas, dari pada lupa mending follow'
+motivation_word = ['twitteran terus sih ayo belajar!', 'woy belajar!', 'belajar belajar belajar belajar!!', 'yok matiin hpnya buka buku!', 'belajar yok belajar', 'ipk bukan penentu kesuksesan sih, tapi yaudah belajar!!']
+message = 'Inget bro\nSenin tgl 9 Basdat jam 1 \nRabu tgl 11 SO / OS jam 10 \nJumat tgl 13 SG jam 7.30 \nSenin tgl 16 Jarkomdat jam 1\nSelasa tgl 17 Matdislog jam 10\nRabu tgl 18 ISIS jam 7.30\nKamis tgl 19 Sismik jam 1\nJumat tgl 20 Pemsim jam 8.30'
+    
